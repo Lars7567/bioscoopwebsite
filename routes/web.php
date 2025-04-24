d00e760d66af8c5d3c6c474309afcba6f2f0f9f2
Lars Mooij <mooij.lars2005@gmail.com>
2025-04-24 11:17:28 +0200

    het aanmaken van de routes voor film

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FilmController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,6 +11,14 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::name('films.')->group(function () {
+    Route::get('/films', [FilmController::class, 'index'])->name('index');
+    Route::get('/films/create', [FilmController::class, 'create'])->name('create');
+    Route::post('/films', [FilmController::class, 'store'])->name('store');
+    Route::get('/films/edit/{film}', [FilmController::class, 'edit'])->name('edit');
+    Route::put('/films/{film}', [FilmController::class, 'update'])->name('update');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
