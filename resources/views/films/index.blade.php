11462b854663c0103daec7b57162519f716411bf
Lars Mooij <mooij.lars2005@gmail.com>
2025-05-08 11:02:57 +0200

    het verwijderen van data omdat deze data op mijn show pagina getoond wordt

--- resources/views/films/index.blade.php
+++ resources/views/films/index.blade.php
@@ -1,16 +1,15 @@
 <x-base-layout>
+
     <div class="container mx-auto px-4 py-8">
         @foreach ($films as $film)
             <div class="film-item bg-white shadow-md rounded-lg p-6 mb-6">
-                <h1 class="text-2xl font-bold text-gray-800 mb-4">{{ $film->title }}</h1>
-                <p class="text-gray-600"><strong>Beschrijving: </strong>{{ $film->beschrijving }}</p>
+                <h1 class="text-2xl font-bold text-gray-800 mb-4"><a href="films/{{ $film->id }}">{{ $film->title }}</a></h1>
                 <p class="text-gray-600"><strong>Duur: </strong>{{ $film->duur }}   Minuten</p>
                 <p class="text-gray-600"><strong>Release datum: </strong>{{ $film->release_datum }}</p>
-                <p class="text-gray-600"><strong>Vereiste leeftijd: </strong>{{ $film->leeftijdskeuring }}</p>
-                <p class="text-gray-600"><strong>Beschikbaarheid: </strong>{{ $film->beschikbaarheid ? 'Ja' : 'Nee' }}</p>
-                <br>
-                <a href="films/edit/{{ $film->id }}" class="btn btn-success btn-sm" style="width: 100px">Bewerken</a>
             </div>
         @endforeach
+        <a href="{{ route('films.create') }}" class="inline-block bg-blue-600 hover:bg-blue-700 text-gray-600 font-semibold py-2 px-4 rounded-lg shadow transition duration-200">
+            Film toevoegen
+        </a>
     </div>
 </x-base-layout>
