44dada1be1a63cee63d233fffb3da04c3dd3138b
Lars Mooij <mooij.lars2005@gmail.com>
2025-05-28 12:45:00 +0200

    hier heb ik en middleware gemaakt zodat je niet op bepaalde paginas kan komen als je geen admin bent dit heb ik gedaan door in mijn migratie een tabel te maken genaamd is_admin en daarin een booling te doen en als het een 1 is is hij een admin en als hij 0 is is hij geen admin die ik heb aangefinkt in de web file

--- routes/web.php
+++ routes/web.php
@@ -6,27 +6,15 @@ use App\Http\Controllers\FilmController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\BookingController;
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\FilmZaalController;
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::name('films.')->group(function () {
-    Route::get('/films', [FilmController::class, 'index'])->name('index');
-    Route::get('/films/create', [FilmController::class, 'create'])->name('create');
-    Route::post('/films', [FilmController::class, 'store'])->name('store');
-    Route::get('/films/{film}', [FilmController::class, 'show'])->name('show');
-    Route::get('/films/{film}/edit', [FilmController::class, 'edit'])->name('edit');
-    Route::put('/films/{film}', [FilmController::class, 'update'])->name('update');
-    Route::get('/films/{film}/delete', [FilmController::class, 'delete'])->name('delete');
+Route::get('/', function () {
+    return view('welcome');
 });
-
-
 
 Route::name('about.')->group(function () {
     Route::get('/about', [AboutController::class, 'index'])->name('index');
@@ -36,10 +24,23 @@ Route::name('contact.')->group(function () {
     Route::get('/contact', [ContactController::class, 'index'])->name('index');
 });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::name('filmzaal.')->group(function () {
+    Route::get('/filmzaal', [FilmZaalController::class, 'index'])->name('index');
+    Route::get('/filmzaal/create', [FilmZaalController::class, 'create'])->name('create');
+    Route::post('/filmzaal', [FilmZaalController::class, 'store'])->name('store');
+    Route::get('/filmzaal/{filmzaal}', [FilmZaalController::class, 'show'])->name('show');
+});
+
+Route::middleware(['auth', 'is_admin'])->group(function () {
+    Route::name('films.')->group(function () {
+        Route::get('/films', [FilmController::class, 'index'])->name('index');
+        Route::get('/films/create', [FilmController::class, 'create'])->name('create');
+        Route::post('/films', [FilmController::class, 'store'])->name('store');
+        Route::get('/films/{film}', [FilmController::class, 'show'])->name('show');
+        Route::get('/films/{film}/edit', [FilmController::class, 'edit'])->name('edit');
+        Route::put('/films/{film}', [FilmController::class, 'update'])->name('update');
+        Route::get('/films/{film}/delete', [FilmController::class, 'delete'])->name('delete');
+    });
 
     Route::name('resevering.')->group(function () {
         Route::get('/resevering', [BookingController::class, 'index'])->name('index');
@@ -47,6 +48,14 @@ Route::middleware('auth')->group(function () {
         Route::post('/resevering', [BookingController::class, 'store'])->name('store');
         Route::delete('/resevering/{resevering}/delete', [BookingController::class, 'delete'])->name('delete');
     });
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 });
 
 require __DIR__ . '/auth.php';
